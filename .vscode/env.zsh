646fb1829810a9d1aef6a1e58fd579203953f593
Esonhugh <esonhughoutside@gmail.com>
2023-12-13 15:39:00 +0800

    update env for empty domain

--- .vscode/env.zsh
+++ .vscode/env.zsh
@@ -1,8 +1,18 @@
 export RHOST=10.10.X.X
-export LHOST=10.X.X.X
-export TARGET=xxx.domain.com
+export LHOST=10.10.X.X
+export DOMAIN=
 
 
-export PROJECT_WEB_DELIVERY=$PROJECT_FOLDER/.web-delivery
+## set systembased settings
+export HOST=${DOMAIN:-${RHOST}} # if DOMAIN is not set, use RHOST
+export host=${HOST} # alias as HOST
+export TARGET=${HOST} # target is HOST
 
-unset SSS_LOADED
+export IP=${RHOST} # alias rhost
+export ip=${IP} # alias as IP
+
+export PROJECT_WEB_DELIVERY=$PROJECT_FOLDER/.web-delivery # web-delivery is a folder in PROJECT_FOLDER
+unset SSS_LOADED # make sure sss init shell is not set
+
+# if you need to set functions definitions, you can do it here
+# source $PROJECT_FOLDER/.vscode/utils.zsh
