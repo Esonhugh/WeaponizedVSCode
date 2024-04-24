c8a830c294e9625c3ef8a6edeb11886676338c35
Esonhugh <esonhughoutside@gmail.com>
2024-04-24 11:56:53 +0800

    update: fix bug of shell syntax

--- launch_helper.zsh
+++ launch_helper.zsh
@@ -1,7 +1,7 @@
 weapon_vscode_launch_helper () {
     if [ -n "$PROJECT_FOLDER" ]; then
         # do anything if PROJECT_FOLDER is set
-        if [ -d "$PROJECT_FOLDER/venv"]; then
+        if [ -d "$PROJECT_FOLDER/venv" ]; then
             source $PROJECT_FOLDER/venv/bin/activate
         fi
     fi
